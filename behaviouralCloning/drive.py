02d7c3e4b568e7f97f1d04b6b2715995c37847e3
Jun Chao <junchao530@gmail.com>
2024-02-01 00:04:58 -0700

    completed version

--- behaviouralCloning/drive.py
+++ behaviouralCloning/drive.py
@@ -1,24 +1,29 @@
-from flask import Flask
-import numpy as np
 import socketio
 import eventlet
+import numpy as np
+from flask import Flask
 from keras.models import load_model
 import base64
 from io import BytesIO
 from PIL import Image
 import cv2
+import os 
 sio = socketio.Server()
-app = Flask(__name__)
+ 
+app = Flask(__name__) #'__main__'
 speed_limit = 10
 
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 def img_preprocess(img):
-    img = img[60:135:,:]
+    img = img[60:135,:,:]
     img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    img = cv2.GaussianBlur(img, (3,3), 0 )
-    img =  cv2.resize(img, (200, 66))
+    img = cv2.GaussianBlur(img,  (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
     img = img/255
     return img
-
+ 
+ 
 @sio.on('telemetry')
 def telemetry(sid, data):
     speed = float(data['speed'])
@@ -30,20 +35,22 @@ def telemetry(sid, data):
     throttle = 1.0 - speed/speed_limit
     print('{} {} {}'.format(steering_angle, throttle, speed))
     send_control(steering_angle, throttle)
-
+ 
+ 
+ 
 @sio.on('connect')
 def connect(sid, environ):
-    print('connected')
+    print('Connected')
     send_control(0, 0)
-
+ 
 def send_control(steering_angle, throttle):
-    print('Sending control:', steering_angle, throttle)
-    sio.emit('steer', data={
-        'steering_angle': str(steering_angle),
-        'throttle': str(throttle)
+    sio.emit('steer', data = {
+        'steering_angle': steering_angle.__str__(),
+        'throttle': throttle.__str__()
     })
-
+ 
+ 
 if __name__ == '__main__':
-    model = load_model('model.h5')
-    app= socketio.Middleware(sio, app)
+    model = load_model('model3.h5')
+    app = socketio.Middleware(sio, app)
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
